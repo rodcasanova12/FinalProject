e3cb2690037ed7fa6be09b109deff984709dcf5e
Rodrigo Casa Nova <60192482+rodcasanova12@users.noreply.github.com>
2021-05-03 11:40:54 -0400

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,8 +1,8 @@
 #Name: Rodrigo Casa Nova
-# Import module
+# Import modules
 from tkinter import *
 import webbrowser
-
+import json
 
 class userInterface():
     def __init__(self):
@@ -24,24 +24,36 @@ class userInterface():
         # Display image
         self.canvas1.create_image(0, 0, image=self.bg,
                              anchor="nw")
+        # Save contact list in json format
+        def getJSON(filePathAndName):
+            with open(filePathAndName, 'r') as fp:
+                return json.load(fp)
 
-        #Url with informantion
+        myObj = getJSON('Contact Info.json');
+
+        # Functions to give each button what to do
         def openWhatIsDepression():
             webbrowser.open('https://www.nimh.nih.gov/health/topics/depression/index.shtml')
         def openCrisis():
             webbrowser.open('https://www.nami.org/Your-Journey/Living-with-a-Mental-Health-Condition/What-to-Do-In-a-Crisis')
         def openHowToStop():
             webbrowser.open('https://www.medicalnewstoday.com/articles/325513#changes-in-sleep-habits')
-        def openContactList():
+        def openAddContactList():
             import MyApp
             MyApp.MyApp().run()
+        def openContactList():
+            print(myObj)
+
+
+
 
 
         # Create Buttons
         self.button1 = Button(self.root, text="What is Depression?", command=openWhatIsDepression)
-        self.button3 = Button(self.root, text="How to Spot Someone With Issues", command=openHowToStop)
+        self.button3 = Button(self.root, text="How to Spot Some With Issues", command=openHowToStop)
         self.button2 = Button(self.root, text="Crisis", command=openCrisis)
-        self.button4 = Button(self.root, text="Contact List", command=openContactList)
+        self.button4 = Button(self.root, text="Add Contact", command=openAddContactList)
+        self.button5 = Button(self.root, text="Contact List", command=openContactList)
 
 
 
@@ -59,6 +71,9 @@ class userInterface():
 
         self.button4_canvas = self.canvas1.create_window(100, 100, anchor="nw",
                                                window=self.button4)
+
+        self.button5_canvas = self.canvas1.create_window(100, 130, anchor="nw",
+                                                         window=self.button5)
 
         # Execute tkinter
         self.root.mainloop()
